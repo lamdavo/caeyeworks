19c75848c135413ee04842adca7db16a9c742149
Lam <vo2022@protonmail.com>
2023-08-03 13:04:32 -0700

    initial

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import { Navbar } from '@/components/ui/navbar/Navbar';
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'NextJS 13 Demo',
+  description: 'with Tailwind CSS 2.2 and Next 13',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang='en'>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ components/ui/navbar/Navbar.tsx
@@ -0,0 +1,68 @@
+import Image from 'next/image';
+import header_logo from '../../../public/images/header_logo.png';
+
+const links = [
+  {
+    id: 1,
+    url: '/about',
+    text: 'ABOUT US',
+  },
+  {
+    id: 2,
+    url: '/',
+    text: 'EYE EXAMS',
+  },
+  {
+    id: 3,
+    url: '/',
+    text: 'SPECIALTY EYE CARE',
+  },
+  {
+    id: 4,
+    url: '/',
+    text: 'BRANDS',
+  },
+  {
+    id: 5,
+    url: '/',
+    text: 'ORDER CONTACTS',
+  },
+  {
+    id: 6,
+    url: '/',
+    text: 'APPOINTMENTS',
+  },
+];
+
+const navWrapper = (
+  <div className='w-full'>
+    <ul className='flex flex-row text-lg'>
+      {links.map((link) => {
+        const { id, url, text } = link;
+        return (
+          <>
+            <li key={id} className='grow'>
+              <a href={url}>{text}</a>
+              <span className='pl-5'>|</span>
+            </li>
+          </>
+        );
+      })}
+    </ul>
+  </div>
+);
+
+const logoWrapper = (
+  <div className='p-6'>
+    <Image src={header_logo} alt='logo' width={110} height={50} />
+  </div>
+);
+
+export const Navbar = () => {
+  return (
+    <div className='max-w-screen-2xl mx-auto h-30 flex items-center bg-blue-950 fixed top-0 left-0 right-0 z-50'>
+      {logoWrapper}
+      {navWrapper}
+    </div>
+  );
+};
